95037a661123e56cb1515b5c81d36a3391ba82c3
elsayedseleim <elsayed_seleim@hotmail.com>
2023-09-15 11:54:39 +0400

    add routes for product and category

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product',function(){
+    return view('product');
+});
+Route::get('/category',function(){
+    return view('category');
+});
